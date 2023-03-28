e6062564b0a9dd0ff822f547a6c51b23b94a7b78
Horlando Leão <horlandoleao8@gmail.com>
2023-03-28 15:44:43 -0300

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,7 +4,7 @@ import { HttpStatusCode } from "solid-start/server";
 export default function NotFound() {
   return (
     <div>
-      <iframe rameborder="0" width="100%" height="800px" src="https://superlogica.cloud.looker.com:19999/login/embed?t=AgEMAABF9wA459vTFu6iAC8BLtwjOeyjgE2-X12Yyl4ux7KyAa6G9YerCjMa0T5eBH82S3ElJ4GncHdndktYZgA_wLStau96P-yb6s8cQh5PnlGniXsFd5Rsz4HEPgPfHsT0KFDFeMOFHCz_yQgIbAhJnXcikJEdYQKyczGqItSOMQBdEHxNgqs7w8y93ERqdWDD3xpCepbvbJMTu-lqJ5Mui2Y6tS-E8bHoEBwR_3ympWdukBp5kDQ71syz3vuGFZnhQ-x5uZOi6j5NXRdNcUaP5--KPnARqVJGr4YsBHygMFi1AbzJwFimWE-L-mhI1FhweBqAaGAyqpZWimXF6yR_E1pL9J4KMMisCBUiqsKMtAJDjbJTCTUYLk9uMPpsu7Ukk1FYaSAMgZ4pcwHn8ZyoHWyIfNiTZBB4V_qYRS38bdKyI9AHTd8duUu4lZDRfbdmcwPZbBwde3zdZF-qPsy9vA"></iframe>
+      <iframe src='https://cloudcelatam.cloud.looker.com/embed/public/RpNtp3PRCH3fDDXdZF5vR3TnXjQjnhhT' width='600' height='338' frameborder='0'></iframe>
     </div>
   );
 }
