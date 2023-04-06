a9ec1c5c39e1f83f41d40caf7c053fefce2dcf0a
Horlando Leão <horlandoleao8@gmail.com>
2023-04-06 10:37:13 -0300

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,7 +4,7 @@ import { HttpStatusCode } from "solid-start/server";
 export default function NotFound() {
   return (
     <div>
-      <iframe src='https://superlogica.cloud.looker.com/embed/public/GtxDt6SQXZVdq7Xf5Qf6SHccBrjhSGJh' width='600' height='338' frameborder='0'></iframe>
+      <iframe src='https://superlogica.cloud.looker.com/embed/public/GtxDt6SQXZVdq7Xf5Qf6SHccBrjhSGJh' width='1200' height='1200' frameborder='0'></iframe>
     </div>
   );
 }
